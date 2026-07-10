68eda06554a7b93dbfe4981922c084db2726f938
RIGDAS22 <nirendas5662@gmail.com>
2026-07-10 16:04:18 +0530

    Add gallery data with all 8 events

--- aikya-website/data/gallery.ts
+++ aikya-website/data/gallery.ts
@@ -1,1 +1,42 @@
-export const galleryData = [];
+export const galleryData = [
+  {
+    id: 1,
+    image: "EVENT1.jpeg",
+    caption: "Bright Beginning",
+  },
+  {
+    id: 2,
+    image: "EVENT2.jpg",
+    caption: "Eradicating Hunger",
+  },
+  {
+    id: 3,
+    image: "EVENT3.jpg",
+    caption: "Hydration Drive",
+  },
+  {
+    id: 4,
+    image: "EVENT4.jpg",
+    caption: "Mother's Day Celebration",
+  },
+  {
+    id: 5,
+    image: "EVENT5.jpeg",
+    caption: "Food Drive",
+  },
+  {
+    id: 6,
+    image: "EVENT6.jpeg",
+    caption: "Volunteers in Action",
+  },
+  {
+    id: 7,
+    image: "EVENT7.jpg",
+    caption: "Creative Minds",
+  },
+  {
+    id: 8,
+    image: "EVENT8.jpg",
+    caption: "Teamed Up",
+  },
+];
