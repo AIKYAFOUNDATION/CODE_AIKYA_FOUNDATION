05e842c8af850230a48ae00f0f2d491ecf4688f2
RIGDAS22 <nirendas5662@gmail.com>
2026-07-10 16:06:23 +0530

    Add GalleryPreview component to homepage

--- aikya-website/app/page.tsx
+++ aikya-website/app/page.tsx
@@ -4,6 +4,7 @@ import MissionPreview from "@/components/home/MissionPreview";
 import WhatWeDoPreview from "@/components/home/WhatWeDoPreview";
 import FounderPreview from "@/components/home/FounderPreview";
 import TeamPreview from "@/components/home/TeamPreview";
+import GalleryPreview from "@/components/home/GalleryPreview";
 import DonatePreview from "@/components/home/DonatePreview";
 import ContactPreview from "@/components/home/ContactPreview";
 
@@ -16,6 +17,7 @@ export default function HomePage() {
       <WhatWeDoPreview />
       <FounderPreview />
       <TeamPreview />
+      <GalleryPreview />
       <DonatePreview />
       <ContactPreview />
     </main>
